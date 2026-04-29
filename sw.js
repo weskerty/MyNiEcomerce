0103e865fb1aba3644803bc26f0f345c5225610d
weskerty <marcoygor0@gmail.com>
2026-04-28 22:56:22 -0300

    Android 28/04/2026 22:56

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V='v28';
+const V='v39';
 const N_ICON='web/otros/Archivos/Imagenes/Permanente/ICONS/ICON.png';
 const N_ICO='web/otros/Archivos/Imagenes/Permanente/ICONS/NOTIFY-MNCM-96x96.png';
 const N_BANNER='web/otros/Archivos/Imagenes/Permanente/ICONS/notif-banner.jpg';
