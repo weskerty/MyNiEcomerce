0ecec801c5f7cbbd4aaf5450fda11bf023fc1d6f
weskerty <marcoygor0@gmail.com>
2026-05-30 23:33:37 -0300

    sync 30/05/2026 23:33

--- functions/api/productos.js
+++ functions/api/productos.js
@@ -4,7 +4,8 @@ export async function onRequest(context){
   const{request,env}=context;
   if(request.method==='OPTIONS')return new Response('',{status:204,headers:H});
   const u=new URL(request.url);
-  const fwd=`${env.SERVER_URL}/productos${u.pathname.replace(/^\/api\/productos/,'')}${u.search}`;
+  const sub=u.pathname.replace(/^\/api\/productos\/?/,'');
+  const fwd=`${env.SERVER_URL}/productos${sub?'/'+sub:''}${u.search}`;
   const init={method:request.method,headers:{'x-bridge-key':env.BRIDGE_KEY,'Content-Type':request.headers.get('Content-Type')||'application/json'}};
   if(request.method!=='GET')init.body=await request.text();
   const res=await fetch(fwd,init);
