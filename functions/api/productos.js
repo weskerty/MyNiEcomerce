08d5780f5dbf6f49aeca970e8825ed5e593090eb
weskerty <marcoygor0@gmail.com>
2026-05-31 00:32:07 -0300

    sync 31/05/2026 00:32

--- functions/api/productos.js
+++ functions/api/productos.js
@@ -1,13 +1,11 @@
-const H={'Access-Control-Allow-Origin':'*','Access-Control-Allow-Methods':'GET,POST,DELETE,OPTIONS','Access-Control-Allow-Headers':'Content-Type'};
+const H={'Content-Type':'application/json','Access-Control-Allow-Origin':'*','Access-Control-Allow-Methods':'GET,POST,DELETE,OPTIONS','Access-Control-Allow-Headers':'Content-Type'};
 
 export async function onRequest(context){
   const{request,env}=context;
   if(request.method==='OPTIONS')return new Response('',{status:204,headers:H});
-  const u=new URL(request.url);
-  const sub=u.pathname.replace(/^\/api\/productos\/?/,'');
-  const fwd=`${env.SERVER_URL}/productos${sub?'/'+sub:''}${u.search}`;
-  const init={method:request.method,headers:{'x-bridge-key':env.BRIDGE_KEY,'Content-Type':request.headers.get('Content-Type')||'application/json'}};
+  const qs=new URL(request.url).search;
+  const init={method:request.method,headers:{'x-bridge-key':env.BRIDGE_KEY,'Content-Type':'application/json'}};
   if(request.method!=='GET')init.body=await request.text();
-  const res=await fetch(fwd,init);
+  const res=await fetch(`${env.SERVER_URL}/productos${qs}`,init);
   return new Response(res.body,{status:res.status,headers:{...H,'Content-Type':res.headers.get('Content-Type')||'application/json'}});
 }
