b2014bf1e7951371cc8d41d710802ac32113ab5b
weskerty <marcoygor0@gmail.com>
2026-07-10 19:25:52 -0300

    sync 10/07/2026 19:25

--- functions/api/productos.js
+++ functions/api/productos.js
@@ -4,7 +4,8 @@ export async function onRequest(context){
   const{request,env}=context;
   if(request.method==='OPTIONS')return new Response('',{status:204,headers:H});
   const qs=new URL(request.url).search;
-  const init={method:request.method,headers:{'x-bridge-key':env.BRIDGE_KEY,'Content-Type':'application/json'}};
+  const ip=request.headers.get('cf-connecting-ip')||'';
+  const init={method:request.method,headers:{'x-bridge-key':env.BRIDGE_KEY,'Content-Type':'application/json','cf-connecting-ip':ip}};
   if(request.method!=='GET')init.body=await request.text();
   const res=await fetch(`${env.SERVER_URL}/productos${qs}`,init);
   return new Response(res.body,{status:res.status,headers:{...H,'Content-Type':res.headers.get('Content-Type')||'application/json'}});
