e48a6b2bd91cb8d806489d2c4effdf7dfcd8456a
weskerty <marcoygor0@gmail.com>
2026-08-20 23:04:24 +0000

    New NO CDN

--- web/scripts/Otros/update.sh
+++ web/scripts/Otros/update.sh
@@ -7,6 +7,9 @@ MiniSearch/index.js|https://cdn.jsdelivr.net/npm/minisearch@7/dist/umd/index.min
 MarkDownIT/markdown-it.min.js|https://cdn.jsdelivr.net/npm/markdown-it@14/dist/markdown-it.min.js
 MarkDownIT/markdownItAnchor.umd.min.js|https://cdn.jsdelivr.net/npm/markdown-it-anchor@9/dist/markdownItAnchor.umd.min.js
 PwaInstall/pwa-install.bundle.js|https://cdn.jsdelivr.net/npm/@khmyznikov/pwa-install@latest/dist/pwa-install.bundle.js
+EasyMDE/easymde.min.js|https://cdn.jsdelivr.net/npm/easymde@2/dist/easymde.min.js
+EasyMDE/easymde.min.css|https://cdn.jsdelivr.net/npm/easymde@2/dist/easymde.min.css
+DOMPurify/purify.min.js|https://cdn.jsdelivr.net/npm/dompurify@3/dist/purify.min.js
 '
 
 echo "$B1" | while IFS='|' read -r C1 C2; do
